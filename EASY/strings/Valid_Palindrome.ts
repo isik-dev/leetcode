b4d6423d20a09a03fc3b1a60c8e8a6a9f79d57a7
islomnumanov <islom4ik.numanov@gmail.com>
2022-08-23 21:22:27 +0900

    [easy-string] added BFS for valid palindrome

--- EASY/strings/Valid_Palindrome.ts
+++ EASY/strings/Valid_Palindrome.ts
@@ -13,19 +13,32 @@
 // -------------------------------- Brute Force Solution -------------------------------- //
 
 // - using toLowerCase() to turn s into lowercase
-// - using \W Metacharacter to remove non alphanumeric chars
+// - using \W Metacharacter to remove non alphanumeric chars (however underscore is considered to be an alphanumeric character)
 // - using A-za-z0-9 regex to remove non alphanumeric chars: replace(/[^A-za-z0-9]/g, '')
 // - using replace() to replace non alphanumeric chars with an empty string
 
-// regular Expression usage: regex structure is //g
-// for instance: /\W/g
-// for instance: /[^A-za-z0-9]/g
+// regular Expression usage: regex structure is //
+// for instance: [^\W]
+// for instance: [^A-Za-z0-9]
+// g should be used for a global search inside that regex
 
-function isPalindrome(s: string) {
-  const init = s.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
-
-  const reverse = init.split('').reverse().join('');
-  return reverse;
-}
+function isPalindrome(s: string): boolean {
+    const filtered = s.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+    const reverse = filtered.split('').reverse().join('');
+    
+    return filtered === reverse;
+};
 
 console.log(isPalindrome('ab_a'));
+
+
+// -------------------------------- Faster Solution With Pointers -------------------------------- //
+
+// function superSonicPalindromeChecker(s: string) {
+//   let pointer = 0;
+//   let pointer2 = s.length - 1;
+
+//   while (pointer < pointer2) {
+//     if ()
+//   }
+// }
